cc8297e0cf6cd04274d554ba697eebe1a05cd7fd
Arkar Maung <rkarmaung@gmail.com>
2020-07-02 16:03:19 +0800

    Change title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Employee Page
                 </div>
 
                 <div class="links">
